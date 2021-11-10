cdf27e3940d680621beb8af0f55cbb01b07282e5
bugarcic <baronvie@gmail.com>
2021-11-10 10:27:26 -0500

    v2

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const imgUrl = 'https://i.imgur.com/WTHBUgm.png';
-const Marker = ({ icon }) => <img height={'80px'} width={'80px'} src={imgUrl}></img>;
+const Marker = ({ icon }) => <img className="marker"height={'100px'} width={'100px'} src={imgUrl}></img>;
 
 const Map = ({ midpoint }) => {
   return (
@@ -16,13 +16,14 @@ const Map = ({ midpoint }) => {
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyAG8pD29eYb7EnZNrNFinFbmMtJiqqnzKI' }}
           defaultCenter={midpoint}
-          defaultZoom={14}>
+          defaultZoom={12}>
           {/* do markers go in here? */}
-          <Marker className="marker" lat={midpoint.lat} lng={midpoint.lng} text='midpoint'/>
+          <Marker lat={midpoint.lat} lng={midpoint.lng} text='midpoint' />
         </GoogleMapReact>
       </div>
     </div>
   )
 }
+
 
 export default Map;
