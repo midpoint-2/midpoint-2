8232461861edc0749f1bf9a69f66b05c746a987a
bugarcic <baronvie@gmail.com>
2021-11-11 11:13:05 -0500

    v

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const imgUrl = 'https://i.imgur.com/WTHBUgm.png';
-const Marker = ({ icon }) => <img className="marker"height={'100px'} width={'100px'} src={imgUrl}></img>;
+const Marker = ({ icon }) => <img className="marker" src={imgUrl}></img>;
 
-const Map = ({ midpoint }) => {
+const Map = (props) => {
   return (
 
     <div className='mapContainer'>
-      {console.log('we are in maps', midpoint.lat, midpoint.lng)}
-      {console.log('is middle point an object', midpoint)}
+      {console.log('we are in maps', props.midpoint.lat, props.midpoint.lng)}
+      {console.log('is middle point an object', props.midpoint)}
 
 
       <div id="map-container" className='mapStyles'>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyAG8pD29eYb7EnZNrNFinFbmMtJiqqnzKI' }}
-          defaultCenter={midpoint}
-          center={midpoint}
+          defaultCenter={props.midpoint}
+          center={props.midpoint}
           defaultZoom={12}>
           {/* do markers go in here? */}
-          <Marker lat={midpoint.lat} lng={midpoint.lng} text='midpoint' />
+          <Marker lat={props.midpoint.lat} lng={props.midpoint.lng} text='midpoint' />
+          {props.places.length > 0 && <Marker lat={props.places[0].geometry.location.lat} lng={props.places[0].geometry.location.lng} text='marker' />}
         </GoogleMapReact>
       </div>
     </div>
